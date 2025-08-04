c13e15bc570e169066b417ba2cc3b9ea02e252d7
chdaiaa <p0081820@gmail.com>
2025-08-04 16:40:46 +0800

    + Add JQuery and initialize javascript file

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,16 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+import $ from 'jquery';
+
+function initClickHandler() {
+    // Wait for the DOM to be fully loaded
+    $(document).ready(function() {
+        $('.wordle-game-input-field').on('click', function() {
+            
+        });
+    });
+}
+
+export default initClickHandler;
