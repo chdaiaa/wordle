8c57059073442e9cfb04c68e49982295ab519b1d
chdaiaa <p0081820@gmail.com>
2025-08-04 15:59:43 +0800

    + Create new table with pg local env setup

---
+++ app/models/wordle/record.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Wordle
+  class Record < ApplicationRecord
+
+    # region DB
+    self.table_name = "wordle_records"
+    # endregion DB
+
+
+    # region Validations
+    # endregion Validations
+
+
+  end
+end
