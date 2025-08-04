97afbc9b0d6e9972ee5c36177f62aeb7bcf1dcca
chdaiaa <p0081820@gmail.com>
2025-08-04 16:40:46 +0800

    + Add JQuery and initialize javascript file

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+
+// This is an asset manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+//= link_tree ../images
+//= link_tree ../stylesheets .css
+//= link_tree ../../javascript
+//= link application.js
+//= link application.css
+
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import initClickHandler from './controllers/index';
+
+// Initialize event handlers
+initClickHandler();
